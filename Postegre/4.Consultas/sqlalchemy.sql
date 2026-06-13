bd8fc703dfff877a7ff44dab39f55f6498a08a56
jc-86 <joaoclebio.camoes@gmail.com>
2026-06-12 22:26:02 -0300

    Criando a tabela

--- Postegre/4.Consultas/sqlalchemy.sql
+++ Postegre/4.Consultas/sqlalchemy.sql
@@ -1,3 +1,14 @@
+-- Criando a tabela do projeto
+CREATE TABLE IF NOT EXISTS anac_sqlalchemy (
+		Numero_da_Ocorrencia INT,
+		Classificacao_da_Ocorrencia VARCHAR(50),
+		Data_da_Ocorrencia DATE,
+		Municipio VARCHAR(50),
+		UF VARCHAR(30),
+		Regiao VARCHAR(30),
+		Nome_do_Fabricante VARCHAR(100)		
+)
+
 SELECT EXTRACT (YEAR FROM 'Data_da_Ocorrencia') AS ano, *
 FROM anac_sqlalchemy
 
